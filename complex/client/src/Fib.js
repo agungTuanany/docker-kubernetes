da0afd8a05c98ce76a5e1e5ac3cb96d08dfcdd29
agungTuanany <agung.tuanany@gmail.com>
2019-08-10 10:39:58 +0700

    Building a Multi Container Apps | fetching Data in the React App.

---
+++ complex/client/src/Fib.js
@@ -0,0 +1,28 @@
+import React, ( Component ) from 'react';
+import axios from 'axios';
+
+
+class Fib extends Component {
+  state = {
+    seenIndexes: [],
+    values: {},
+    index: ''
+  };
+
+  componentDidMount() {
+    this.fetchValues();
+    this.fetchIndexes();
+  }
+
+  async fetchValues() {
+    const values = await axios.get('/api/values/current');
+    this.setState({ values: values.data });
+  }
+
+  async fetchIndexes() {
+    const seenIndexes = await axios.get('/api/values/all');
+    this.setState({
+      seenIndexes: seenIndexes.data
+    });
+  }
+}
---
+++ complex/client/src/OtherPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+export default () => {
+  return (
+    <div>
+      I'm some other page!
+      <Link to='/'>Go back home</link>
+    </div>
+  );
+};
+
+
