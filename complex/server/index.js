bc7589cf5aae88728305e9fb1a4bc4a80ac9279f
agungTuanany <agung.tuanany@gmail.com>
2019-08-10 08:41:58 +0700

    Building a Multi Container Apps | create express router

--- complex/server/index.js
+++ complex/server/index.js
@@ -1,9 +1,10 @@
 const keys = require('./keys');
-
 // Express App Setup
 const express = require('express');
+const { Pool } = require('pg');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const redis = require('redis');
 
 const app = express();
 
@@ -11,8 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-// POstgres Client Setup
-const { Pool } = require('pg');
+// Postgres Client Setup
 const pgCLient = new Pool({
   user: keys.pgUser,
   host: keys.pgHost,
@@ -27,3 +27,56 @@ pgCLient.on('error', () => console.og('PG connection was disrupted with error'))
 pgClient
   .query('CREATE TABLE IF NOT EXIST values (number INT)')
   .catch(err => console.log(err));
+
+
+// Redis Client Setup
+const redisClient = redis.createClient({
+  host: kyes.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+
+const redisPublisher = redisClient.duplicate();
+
+// Express route handles
+app.get('/', (req, res) => {
+  res.send('hi');
+});
+
+app.get('/values/all', async (req, res) => {
+  const value = await pgCLient.query('SELECT * from values');
+
+  res.send(values.rows);
+});
+
+/* QUICK NOTE !!
+ *
+ * we doing a very classic callback without 'await'
+ * cause the redis library for NodeJS doesn't have out of box 'promise support'
+ */
+app.get('values/current', async (req, res) => {
+  redisCLient.hgetall('values', (err, values) => {
+    res.send(values);
+  });
+});
+
+app.post('/values', aynsc (req, res) => {
+  const index = req.body.value.index;
+
+  if (parseInt(index) => 40) {
+    return res.status(422).send('Index too high');
+  }
+
+  redisClient.hset('values', index, 'Nothing yet!');
+  redisPublisher.publish('insert', index);
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+  res.send({ working: true });
+});
+
+const port = process.env.PORT | 5002
+app.listen(port, err => {
+  console.log(Date());
+  console.log(`app was run in port ${port}`)
+
+});
