9516bbb6c6d42eb5f874ea0dee0fbb9659b510eb
agungTuanany <agung.tuanany@gmail.com>
2019-08-10 10:56:17 +0700

    Building a Multi Container Apps | Rendering Logic in the App

--- complex/client/src/Fib.js
+++ complex/client/src/Fib.js
@@ -25,4 +25,51 @@ class Fib extends Component {
       seenIndexes: seenIndexes.data
     });
   }
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios.post('/api/values', {
+      index: this.state.index
+    });
+    this,setState({ index: '' });
+  }
+
+  renderSeenIndexes() {
+    return this.state.seenIndexes.map(({ number }) => number ).join(', ');
+  }
+
+  renderValues() {
+    const entries = [];
+
+    for (let key in this.state.values) {
+      entries.push(
+        <div key={key}>
+          for index {key} I calculated {this.state.values[key]}
+        </div>
+      );
+    }
+
+    return entries;
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Enter your index:</label>
+          <input
+            values={this.state.index}
+            onChange={event => this.setState({ index: event.target.value })}
+          />
+          <button>Submit</button>
+        </form>
+
+        <h3>Indexes I have seen:</h3>
+        { this.renderSeenIndexes() }
+        <h3>Calculated Values:</h3>
+        { this.renderValues() }
+      </div>
+    )
+  }
 }
